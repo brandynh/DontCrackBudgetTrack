b7e9115ba0e33731f0808ad4e9213333db54c748
Brandyn <hufferbrandyn@gmail.com>
2022-01-18 20:09:07 -0600

    set up starter code and configure server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT | 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/budget", {
+
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
+
