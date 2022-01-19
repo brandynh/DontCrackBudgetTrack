acba8cee3c8c33fd37298d5e58abdc25c4d5ba6e
Brandyn <hufferbrandyn@gmail.com>
2022-01-19 04:27:32 -0600

    sever adjustments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -16,14 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgetTrack", {
-
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-
-});
+  process.env.MONGODB_URI || "mongodb://localhost/transactions",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
@@ -31,4 +31,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT}`);
 });
-
